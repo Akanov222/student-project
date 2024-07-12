65728a657af5a0906ffd84c924c7fb7ccf7bb1ee
Akanov222 <akanov.gazproekt@gmail.com>
2024-07-12 16:04:01 +0300

    First commit

---
+++ src/SaveStudentOrder.java
@@ -0,0 +1,18 @@
+public class SaveStudentOrder {
+
+    static void scheduleStudentOrder() {
+        System.out.println("scheduleStudentOrder");
+    }
+
+    static void  financeStudentOrder() {
+        System.out.println("financeStudentOrder");
+    }
+
+    public static void main(String[] args) {
+       saveStudentOrder();
+    }
+
+    public static void saveStudentOrder() {
+        System.out.println("saveStudentOrder");
+    }
+}
