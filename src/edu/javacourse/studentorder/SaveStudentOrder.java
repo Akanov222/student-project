2460b6c997b9a065080e78715b3d649dbc3dd215
Akanov222 <akanov.gazproekt@gmail.com>
2024-07-23 19:33:11 +0300

    Lessons 19-20

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -21,9 +21,16 @@ public class SaveStudentOrder {
     public static StudentOrder buildStudentOrder(long id) {
         StudentOrder so = new StudentOrder();
         so.setStudentOrderId(id);
+        StudentOrder so1 = so;
+        printStudentOrder(so1);
 
         Adult husband = new Adult("Vasilev",
                 "Andru", "Petrovich", null);
         return so;
+    }
+
+    static void printStudentOrder(StudentOrder stOr)
+    {
+        System.out.println(stOr.getStudentOrderId());
     }
 }
