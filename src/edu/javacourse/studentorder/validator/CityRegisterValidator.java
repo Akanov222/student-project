9203fa8db595da709a4853522c0e4b84fe98ae16
Akanov222 <akanov.gazproekt@gmail.com>
2024-08-06 16:50:45 +0300

    Lessons 27-28

--- src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -4,6 +4,7 @@ import edu.javacourse.studentorder.domain.Child;
 import edu.javacourse.studentorder.domain.Person;
 import edu.javacourse.studentorder.domain.register.AnswerCityRegister;
 import edu.javacourse.studentorder.domain.register.AnswerCityRegisterItem;
+import edu.javacourse.studentorder.exeption.TransportException;
 import edu.javacourse.studentorder.validator.register.CityRegisterChecker;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.domain.StudentOrder;
@@ -14,10 +15,7 @@ import java.util.List;
 
 public class CityRegisterValidator {
 
-    public String hostName;
-    protected  int port;
-    private String login;
-    String password;
+    public static final String IN_CODE = "NO GRN";
 
     private CityRegisterChecker personChecker;
 
@@ -36,11 +34,29 @@ public class CityRegisterValidator {
     }
 
     private AnswerCityRegisterItem checkPerson(Person person) {
+
+        AnswerCityRegisterItem.CityStatus status;
+        AnswerCityRegisterItem.CityError error = null;
         try {
-            CityRegisterResponse hans = personChecker.checkPerson(person);
-        } catch (CityRegisterException e){
-            e.printStackTrace(System.out);
+            CityRegisterResponse tmp = personChecker.checkPerson(person);
+            status = tmp.isExisting()?
+                    AnswerCityRegisterItem.CityStatus.YES :
+                    AnswerCityRegisterItem.CityStatus.NO;
+        } catch (CityRegisterException ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.EROR;
+            error = new AnswerCityRegisterItem.CityError(ex.getCode(), ex.getMessage());
+        }  catch (TransportException ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.EROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.EROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
         }
-        return null;
+        AnswerCityRegisterItem ans =
+                new AnswerCityRegisterItem(status, person, error);
+        return ans;
     }
 }
--- src/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
+++ src/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
@@ -2,10 +2,12 @@ package edu.javacourse.studentorder.validator.register;
 
 import edu.javacourse.studentorder.domain.Person;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
+import edu.javacourse.studentorder.exeption.CityRegisterException;
+import edu.javacourse.studentorder.exeption.TransportException;
 
 public class RealCityRegisterChecker implements CityRegisterChecker{
 
-    public CityRegisterResponse checkPerson(Person person) {
+    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException, TransportException {
         return null;
     }
 }
